4bdd7b63e1b05f0da453aca0a27f79d6113afb92
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-09-01 18:00:31 +0200

    Update main.tf

--- modules/network/modules/subnet/main.tf
+++ modules/network/modules/subnet/main.tf
@@ -11,6 +11,8 @@ terraform {
 locals {
   vcn = var.vcn
   cidr = var.cidr
+  name = var.name
+  exposed = var.exposed
   
   subnet = oci_core_subnet.subnet
 }
@@ -19,5 +21,8 @@ resource "oci_core_subnet" "subnet" {
   compartment_id = local.vcn.compartment_id
   vcn_id = local.vcn.id
   
+  display_name = local.name
   cidr_block = local.cidr
+  
+  prohibit_internet_ingress = !local.exposed
 }
