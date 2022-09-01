3c632a50548b64e79eeacb0cd67c18aab461a672
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-09-01 14:57:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,3 +8,17 @@ terraform {
   }
 }
 
+module "governance" {
+  source = "./modules/governance"
+  providers = {
+    oci = oci.home
+  }
+  
+  parent_compartment = var.oci_tenancy_id
+  name = "sisalfunclub-test"
+  description = "Sisal Fun Club - Test Enviroment"
+  
+  environment = "test"
+  app_code = "nsfc"
+  factory = "Digital"
+}
