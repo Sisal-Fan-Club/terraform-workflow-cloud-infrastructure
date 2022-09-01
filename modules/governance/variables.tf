03d6526ec7d0a80dd3ef98eb0b708deff09fd01b
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-09-01 14:58:39 +0200

    Update variables.tf

--- modules/governance/variables.tf
+++ modules/governance/variables.tf
@@ -1,15 +1,3 @@
-locals {
-  root_compartment = var.root_compartment
-  name = var.name
-  description = var.description
-  
-  freeform_tags = {
-    app-code = var.app_code
-    factory = var.factory
-    environment = var.environment
-  } 
-}
-
 variable "parent_compartment" {
   type = string
 }
