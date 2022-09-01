1923878a766ab0faacf8ac626924e06ae5756fc1
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-09-01 14:48:22 +0200

    Update main.tf

--- modules/governance/main.tf
+++ modules/governance/main.tf
@@ -7,3 +7,27 @@ terraform {
     }
   }
 }
+
+locals {
+  root_compartment = var.root_compartment
+  name = var.name
+  description = var.description
+  
+  freeform_tags = {
+    app-code = var.app_code
+    factory = var.factory
+    environment = var.environment
+  } 
+}
+
+resource "oci_identity_compartment" "compartment" {
+  compartment_id = local.root_compartment
+  
+  name = local.name
+  description = local.description
+  enable_delete = true
+  
+  freeform_tags = merge({
+    managed-by = "Terraform"
+  }, local.freeform_tags)
+}
