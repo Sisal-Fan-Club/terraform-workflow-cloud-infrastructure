584db2751d5d540a14e770b8866f362206a545c0
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-09-02 11:29:11 +0200

    Update main.tf

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -7,3 +7,26 @@ terraform {
     }
   }
 }
+
+locals {
+  vcn = var.vcn
+}
+
+resource "oci_core_security_list" "disabled" {
+  compartment_id = local.vcn.compartment_id
+  vcn_id = local.vcn.id
+  
+  display_name = "Disabled Security List"
+  
+  ingress_security_rules {
+    description = "Allow all traffic TO the subnet"
+    protocol = "all"
+    source = "0.0.0.0/0"
+  }
+  
+  egress_security_rules {
+    description = "Allow all traffic FROM the subnet"
+    protocol = "all"
+    destination  = "0.0.0.0/0"
+  }
+}
