19bd55bb0317c78ccb00ed9d41f730d5e26da62b
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-09-02 12:27:40 +0200

    Update main.tf

--- modules/security/modules/profiles/main.tf
+++ modules/security/modules/profiles/main.tf
@@ -11,6 +11,8 @@ terraform {
 locals {
   vcn = var.vcn
   name = var.name
+  
+  profile = oci_core_network_security_group.profile
 }
 
 resource "oci_core_network_security_group" "profile" {
@@ -19,3 +21,15 @@ resource "oci_core_network_security_group" "profile" {
   
   display_name = local.name
 }
+
+resource "oci_core_network_security_group_security_rule" "default_ingress" {
+  network_security_group_id = local.profile.id
+  
+  description = "Allow all traffic in same Security Profile"
+  
+  direction = "INGRESS"
+  protocol = "all"
+  source_type = "NETWORK_SECURITY_GROUP"
+  source = local.profile.id
+}
+
