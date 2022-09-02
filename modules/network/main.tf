233dc4e237605de2408e47b4555d9105916a89b4
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-09-02 20:09:58 +0200

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -44,43 +44,6 @@ resource "oci_core_nat_gateway" "ngw" {
   
   freeform_tags = merge({}, local.vcn.freeform_tags)
 }
-
-data "oci_core_services" "oci_services" {
-  filter {
-    name = "name"
-    values = ["^All .* Services In Oracle Services Network"]
-    regex = true
-  }
-}
-
-resource "oci_core_service_gateway" "sgw" {
-  compartment_id = local.vcn.compartment_id
-  vcn_id = local.vcn.id
-
-  display_name = "OCI Backbone Gateway"
-
-  dynamic "services" {
-    for_each = { for service in data.oci_core_services.oci_services.services: 
-      service.id => service
-    }
-
-    content {
-      service_id = services.value.id
-    }
-  }
-}
-
-locals {
-  route_oci_services = {
-    for service in data.oci_core_services.oci_services.services:
-      service.id => {
-        description = ""
-        destination = service.cidr_block
-        destination_type = "SERVICE_CIDR_BLOCK"
-        gateway = oci_core_service_gateway.sgw.id
-      }
-  }
-}
   
 module "subnet" {
   for_each = local.subnets
@@ -95,9 +58,7 @@ module "subnet" {
   name = each.key
   cidr = each.value.cidr
   exposed = each.value.exposed
-  
-  routes = merge({
-  }, local.route_oci_services)
+
   default_gateway_id = each.value.exposed ? oci_core_internet_gateway.igw.id : oci_core_nat_gateway.ngw.id
 }
   
