fc20b01bc8170b5873696af1e770fca0e3a40fea
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-09-06 18:08:45 +0200

    Update main.tf

--- modules/kubernetes/modules/nodepool/main.tf
+++ modules/kubernetes/modules/nodepool/main.tf
@@ -1,4 +1,5 @@
-# Terraform global configuration
+#
+Terraform global configuration
 terraform {
   required_providers {
     oci = {
@@ -6,4 +7,29 @@ terraform {
       version = ">= 4"
     }
   }
+}
+
+locals {
+  cluster = var.cluster
+  
+  ads = data.oci_identity_availability_domains.availability_domains.availability_domains
+}
+
+resource "oci_containerengine_node_pool" "nodepool" {
+  compartment_id = local.cluster.compartment_id
+  cluster_id = local.cluster.id
+  
+  name = local.cluster.name
+  kubernetes_version = local.cluster.kubernetes_version
+  size = 3
+  
+  node_shape = "VM.Standard.E4.Flex"
+  node_shape_config {
+    ocpus = 1
+    memory_in_gbs = 1
+  }
+}
+
+data "oci_identity_availability_domains" "availability_domains" {
+  compartment_id = local.cluster.compartment_id 
 }
