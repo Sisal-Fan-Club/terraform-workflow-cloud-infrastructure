03762eb50d7a8078293df4e4048d6af247f1124b
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-09-06 11:06:23 +0200

    Update main.tf

--- modules/kubernetes/main.tf
+++ modules/kubernetes/main.tf
@@ -8,3 +8,28 @@ terraform {
   }
 }
 
+locals {
+  compartment = var.compartment
+  subnet_pods = var.subnet_pods
+  
+  version = var.kubernetes_version
+}
+
+resource "oci_containerengine_cluster" "kubernetes" {
+  compartment_id = local.compartment.id
+  vcn_id = local.subnet_pods.vcn_id
+  
+  name = local.compartment.name
+  kubernetes_version = local.version
+  
+  cluster_pod_network_options {
+    cni_type = "OCI_VCN_IP_NATIVE"
+  }
+  
+  options {
+    kubernetes_network_config {
+      pods_cidr = local.subnet_pods.cidr_block
+      services_cidr = "10.96.0.0/16"
+    }
+  }
+}
