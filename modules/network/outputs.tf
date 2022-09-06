9f81ff8aa41a3ca4f23569a180c1344f38a63d51
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-09-06 17:12:56 +0200

    Update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,4 +1,8 @@
 output "vcn" {
+  depends_on = [
+    module.subnet
+  ]
+    
   value = merge({
     subnets = { for subnet_name, subnet_module in module.subnet: subnet_name => subnet_module.subnet }
   }, local.vcn)
